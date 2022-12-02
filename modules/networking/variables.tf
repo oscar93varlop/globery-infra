5e85719a65e60108e7f188ff8e3dd948b169b6ed
oscar vargas <oscaradolfo.vargas@globant.com>
2022-12-02 16:29:45 -0500

    testeando bloqueaar variables en networking.tf en dev/networking.tf

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,12 +1,3 @@
-/* variable "cidr_ab" {
-    type = map
-    default = {
-        dev             = "172.22"
-        qa              = "172.24"
-        staging         = "172.26"
-        production      = "172.28"
-    }
-} */
 
 variable "env" {
 }
@@ -26,9 +17,6 @@ variable "cidr_block_sbnt_private_3" {
 }
 variable "cidr_block_sbnt_private_4" {
 }
-# variable "env" {
-#     description = "environment : dev or prod"
-# }
 
 variable "az1" {
   description = "zona de disponibilidad 2"
@@ -38,19 +26,3 @@ variable "az2" {
   description = "zona de disponibilidad 2"
   default = "us-east-1b"
 }
-/* variable "az3" {
-  description = "zona de disponibilidad 3"
-  default = "us-east-1c"
-}
-variable "az4" {
-  description = "zona de disponibilidad 2"
-  default = "us-east-1d"
-}
-variable "az5" {
-  description = "zona de disponibilidad 2"
-  default = "us-east-1e"
-}
-variable "a63" {
-  description = "zona de disponibilidad 3"
-  default = "us-east-1f"
-} */
