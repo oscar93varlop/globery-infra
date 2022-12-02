809786d15663c7285be0679048774167a3929044
oscar vargas <oscaradolfo.vargas@globant.com>
2022-12-02 12:15:27 -0500

    creando vpc int 3

---
+++ environments/dev/variables.tf
@@ -0,0 +1,18 @@
+variable "env" {
+}
+variable "project" {
+}
+variable "cidr_block_dev" {
+}
+variable "cidr_block_sbnt_public_1" {
+}
+variable "cidr_block_sbnt_private_1" {
+}
+variable "cidr_block_sbnt_private_2" {
+}
+variable "cidr_block_sbnt_public_2  " {
+}
+variable "cidr_block_sbnt_private_3" {
+}
+variable "cidr_block_sbnt_private_4" {
+}
