6130921e7e29b892eb4c695430056f51c0ed81cc
oscar vargas <oscaradolfo.vargas@globant.com>
2022-12-02 12:31:00 -0500

    creando vpc int 7

--- environments/dev/networking.tf
+++ environments/dev/networking.tf
@@ -1,5 +1,14 @@
 module "networking" {
     count         = 1   
     source        = "../../modules/networking"
+    env = dev 
+    project = globery
+    cidr_block_dev = "10.1.0.0/16"
+    cidr_block_sbnt_public_1 = "10.1.1.0/24"
+    cidr_block_sbnt_private_1 =  "10.1.3.0/24"
+    cidr_block_sbnt_private_2 = "10.1.4.0/24"
+    cidr_block_sbnt_public_2   = "10.1.2.0/24"
+    cidr_block_sbnt_private_3 =  "10.1.5.0/24"
+    cidr_block_sbnt_private_4 =  "10.1.6.0/24"
     # some_variable = some_value
 }
