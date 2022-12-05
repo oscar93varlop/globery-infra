6522defb108fd7e4939ffa759badc75818c7cd69
oscar vargas <oscaradolfo.vargas@globant.com>
2022-12-05 09:25:30 -0500

    agregando output del module networking error de names

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,7 +1,7 @@
-output "subnet_public" {
+output "subnet_public_1" {
   value = aws_subnet.sbnt_public_1
 }
-output "subnet_public" {
+output "subnet_public_2" {
   value = aws_subnet.sbnt_public_2.id
 }
 output "subnet_private_1" {
