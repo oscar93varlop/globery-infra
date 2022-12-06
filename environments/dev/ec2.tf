369dba03fbefb9f717d1fc5dc0dc6112f046d21b
nicolas <zuluagazuluaga1985@gmail.com>
2022-12-06 11:51:48 -0500

    configuracion de variables EC2

--- environments/dev/ec2.tf
+++ environments/dev/ec2.tf
@@ -1,8 +1,8 @@
 module "ec2"{
 count = 1
 source        = "../../modules/ec2"
-region =
-amis =
-type_instance =
-subnet_public =
+region = "us-east-1"
+amis = "ami-0149b2da6ceec4bb0"
+type_instance = "t2.medium"
+subnet_public = var.cidr_block_sbnt_public_1
 }
