85c743bdf78ef16abce5defcea0e73b99d3ae86a
oscar vargas <oscaradolfo.vargas@globant.com>
2022-12-06 11:54:02 -0500

    testenado runner

--- environments/dev/route53.tf
+++ environments/dev/route53.tf
@@ -3,10 +3,10 @@ count = 1
 source        = "../../modules/r53_zone"
 domain_name = var.domain_name
 }
-/* module "record_dns" {
-count = 1
-source = "../../modules/r53_record"
-zone_id = module.route_zone[0].zone_id
-name =
-depends_on = [module.route_zone]
+/* resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = var.name
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.lb.public_ip]
 } */
